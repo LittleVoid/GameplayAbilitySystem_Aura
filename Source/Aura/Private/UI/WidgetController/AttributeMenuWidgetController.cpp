2a2e6191058d4f67b7b0ab61ed643f1dfd8266df
VoidCraft <08evangelion08@gmail.com>
2025-10-08 16:02:36 +0200

    [+] Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright VoidCraft
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitalValues()
+{
+	
+}
