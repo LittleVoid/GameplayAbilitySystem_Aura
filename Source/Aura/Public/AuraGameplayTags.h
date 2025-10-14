09a552b3232894aea9ad5f4e535fdd4b0a9f5643
VoidCraft <08evangelion08@gmail.com>
2025-10-14 20:51:50 +0200

    [+] Mapping Damage Types to Resistances

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -31,8 +31,14 @@ public:
 	FGameplayTag Attributes_Secondary_HealthRegeneration;
 	FGameplayTag Attributes_Secondary_ManaRegeneration;
 	FGameplayTag Attributes_Secondary_MaxHealth;
-	FGameplayTag Attributes_Secondary_MaxMana;	
+	FGameplayTag Attributes_Secondary_MaxMana;
 
+// Resistance
+	FGameplayTag Attributes_Resistance_Fire;
+	FGameplayTag Attributes_Resistance_Lightning;
+	FGameplayTag Attributes_Resistance_Arcane;
+	FGameplayTag Attributes_Resistance_Physical;
+	
 // Input Tags
 	FGameplayTag InputTag_LMB;
 	FGameplayTag InputTag_RMB;
@@ -44,8 +50,11 @@ public:
 // Damage Types
 	FGameplayTag Damage;
 	FGameplayTag Damage_Fire;
+	FGameplayTag Damage_Lightning;
+	FGameplayTag Damage_Arcane;
+	FGameplayTag Damage_Physical;
 
-	TArray<FGameplayTag> DamageTypes;
+	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
 	
 	FGameplayTag Effects_HitReact;
 	
