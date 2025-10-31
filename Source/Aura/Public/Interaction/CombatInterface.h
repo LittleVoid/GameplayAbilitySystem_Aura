c8318c464099c8c4dc4bb644b000f84e1806ff88
VoidCraft <08evangelion08@gmail.com>
2025-10-31 16:07:01 +0100

    [+] Debuff Niagara Component

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -8,8 +8,11 @@
 #include "AbilitySystem/Data/CharacterClassInfo.h"
 #include "CombatInterface.generated.h"
 
+class UAbilitySystemComponent;
 class UAnimMontage;
 class UNiagaraSystem;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnASCRegistered, UAbilitySystemComponent*)
 
 USTRUCT(BlueprintType)
 struct FTaggedMontage
@@ -81,4 +84,6 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	ECharacterClass GetCharacterClass();
+
+	virtual FOnASCRegistered GetOnASCRegisteredDelegate() = 0;
 };
