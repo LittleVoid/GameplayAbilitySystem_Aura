215b788902125c3970a800d337ead3b78c144d97
VoidCraft <08evangelion08@gmail.com>
2025-11-16 18:02:42 +0100

    [+] Invoke Replicated Event

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -86,4 +86,7 @@ public:
 	ECharacterClass GetCharacterClass();
 
 	virtual FOnASCRegistered GetOnASCRegisteredDelegate() = 0;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetInShockLoop(bool bInLoop);
 };
