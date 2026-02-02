2c2fc126a4bc20a20834548b583d47503ddefd76
VoidCraft <08evangelion08@gmail.com>
2026-02-02 22:26:32 +0100

    [+] Showing Player Level in Load Screen

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -42,11 +42,13 @@ public:
 	
 	void SetPlayerName(FString InPlayerName);
 	void SetMapName(FString InMapName);
+	void SetPlayerLevel(int32 InLevel);
 	void SetLoadSlotName(FString InLoadSlotName);
 
 	FString GetPlayerName() const { return PlayerName; }
 	FString GetLoadSlotName() const { return LoadSlotName; }
 	FString GetMapName() const { return MapName; }
+	int32 GetPlayerLevel() const { return PlayerLevel; }
 
 private:
 	
@@ -58,4 +60,7 @@ private:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess="true"));
 	FString MapName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess="true"));
+	int32 PlayerLevel;
 };
