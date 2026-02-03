8b8322c7a6551126d9d317e2da7825496f3f7194
VoidCraft <08evangelion08@gmail.com>
2026-02-03 21:19:11 +0100

    [+] Beacons

--- Source/Aura/Private/Checkpoint/Checkpoint.cpp
+++ Source/Aura/Private/Checkpoint/Checkpoint.cpp
@@ -65,7 +65,10 @@ void ACheckpoint::BeginPlay()
 {
 	Super::BeginPlay();
 
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+	if (bBindOverlapCallback)
+	{
+		Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+	}
 }
 
 void ACheckpoint::SetMoveToLocation_Implementation(FVector& OutDestination)
@@ -75,7 +78,10 @@ void ACheckpoint::SetMoveToLocation_Implementation(FVector& OutDestination)
 
 void ACheckpoint::HighlightActor_Implementation()
 {
-	CheckpointMesh->SetRenderCustomDepth(true);
+	if (!bReached)
+	{
+		CheckpointMesh->SetRenderCustomDepth(true);
+	}
 }
 
 void ACheckpoint::UnHighlightActor_Implementation()
--- Source/Aura/Public/Checkpoint/Checkpoint.h
+++ Source/Aura/Public/Checkpoint/Checkpoint.h
@@ -27,8 +27,11 @@ public:
 	virtual void LoadActor_Implementation() override;
 	/* end Save Interface */
 
-	UPROPERTY(BlueprintReadOnly, SaveGame)
+	UPROPERTY(BlueprintReadWrite, SaveGame)
 	bool bReached = false;
+
+	UPROPERTY(EditAnywhere)
+	bool bBindOverlapCallback = true;
 protected:
 
 	UFUNCTION()
@@ -51,6 +54,7 @@ protected:
 	UFUNCTION(BlueprintImplementableEvent)
 	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialInstance);
 
+	UFUNCTION(BlueprintCallable)
 	void HandleGlowEffects();
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
